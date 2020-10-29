a6db3644ea5176cd6949f25c836b8b8468266625
Erik Wallin <skelpdar@gmail.com>
2020-10-29 14:07:08 +0100

    Added test script to show how time-axes are configured

---
+++ timeAxis.C
@@ -0,0 +1,49 @@
+#include "TGraph.h"
+
+void temperatureOverTime(){
+
+	auto t = readFile("data/rawdata_smhi-opendata_1_52240_20200905_163726.csv");
+	
+	//This is a simple way to draw directly from a TTree
+	//With limited options
+	//t->Draw("Temperature:Time");
+	//auto graph = (TGraph*)gPad->GetPrimitive("Graph");
+	//graph->GetXaxis()->SetTimeFormat("%Y");
+	
+
+	//Data loading from TTree
+	Double_t temperature = 0;
+	t->SetBranchAddress("Temperature",&temperature);
+	Long64_t N = t->GetEntries();
+
+	Int_t y[N];
+
+	for(Long64_t i=0; i<N; i++){
+		t->GetEntry(i);
+		y[i] = (Int_t)temperature;
+		std::cout << y[i] << std::endl;
+	}
+	
+	Int_t time = 0;
+	t->SetBranchAddress("Time",&time);
+
+	Int_t x[N];
+
+	for(Long64_t i=0; i<N; i++){
+		t->GetEntry(i);
+		x[i] = time;
+		std::cout << x[i] << std::endl;
+	}
+
+	//Drawing
+	TGraph* graph = new TGraph(N,x,y);
+	//Make the x-axis a time axis
+	graph->GetXaxis()->SetTimeDisplay(1);
+	//Start time at EPOCH
+	graph->GetXaxis()->SetTimeOffset(0);
+	//Formatting, here we just show years
+	graph->GetXaxis()->SetTimeFormat("%Y");
+	//Scatter plot on new canvas
+	graph->Draw("ap");
+
+}
